4b3f7d0a1fc72f634a6c3a8c8c40d2c95412ad68
Martijn van der Elsen <m.vanderelsen@evebury.com>
2026-03-07 16:36:28 +0100

    test

--- test/Evebury.Gdsn.Gs1.Test/CatalogueItemNotification.cs
+++ test/Evebury.Gdsn.Gs1.Test/CatalogueItemNotification.cs
@@ -12,32 +12,45 @@ namespace Evebury.Gdsn.Gs1.Test
             message.Load("CatalogueItemNotification.xml");
             Gs1Validator validator = new();
             Response response = await validator.Validate(message);
-            response.Localize(new System.Globalization.CultureInfo("nl"));
-            string transactionId = "";
-            if (response.Transactions != null && response.Transactions.Length >= 1) 
-            {
-                transactionId = response.Transactions[0].Id;
-            }
-            //if transaction id is set message is valid schema and rules are applied
-            Assert.AreEqual("20051101", transactionId);
-            Assert.AreEqual(StatusType.ERROR, response.Status);
+            Assert.AreEqual(StatusType.OK, response.Status, "All validations should pass");
         }
 
         [TestMethod]
-        public async Task Schema()
+        public async Task ApplySchema()
         {
             XmlDocument message = new();
-            message.Load("CatalogueItemNotificationInvalid.xml");
+            message.Load("CatalogueItemNotificationSchema.xml");
             Gs1Validator validator = new();
             Response response = await validator.Validate(message);
-            string transactionId = "";
-            if (response.Transactions != null && response.Transactions.Length >= 1)
+            string eventId = string.Empty;
+            if (response.Transactions != null && response.Transactions.Length > 0)
             {
-                transactionId = response.Transactions[0].Id;
+                Transaction transaction = response.Transactions[0];
+                if (transaction.Events != null && transaction.Events.Length > 0)
+                {
+                    eventId = transaction.Events[0].Id;
+                }
             }
-            //if schema fails transactionId is set to "validation_reponse"
-            Assert.AreNotEqual("20051101", transactionId);
-            Assert.AreEqual(StatusType.ERROR, response.Status);
+            Assert.AreEqual("xml_schema", eventId);
+        }
+
+        [TestMethod]
+        public async Task ApplyRules()
+        {
+            XmlDocument message = new();
+            message.Load("CatalogueItemNotificationRule.xml");
+            Gs1Validator validator = new();
+            Response response = await validator.Validate(message);
+            string eventId = string.Empty;
+            if (response.Transactions != null && response.Transactions.Length > 0)
+            {
+                Transaction transaction = response.Transactions[0];
+                if (transaction.Events != null && transaction.Events.Length > 0)
+                {
+                    eventId = transaction.Events[0].Id;
+                }
+            }
+            Assert.AreEqual("448", eventId);
         }
     }
 }
